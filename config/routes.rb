2abeb092f3237d02c92f6a8355d3449ea44f7aea
Zina Ivashko <zinaida.ivashko@gmail.com>
2023-08-05 16:01:47 +0100

    adding the controllers movies, lists, bookmarks

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # resources :bookmarks, only: [:create, :destroy]
+  resources :lists
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
