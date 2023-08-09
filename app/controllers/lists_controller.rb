b89d12b122a2237643cf25aa4b825ccfe0a91453
Zina Ivashko <zinaida.ivashko@gmail.com>
2023-08-09 09:09:58 +0100

    lists controller changes

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,4 +3,12 @@ class ListsController < ApplicationController
   def index
     @lists = List.all
   end
+
+    def show
+      @bookmark = Bookmark.new
+      @review = Review.new(list: @list)
+    end
+
+
+
 end
